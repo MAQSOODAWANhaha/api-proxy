4ea720fcbaa455e665e1f38254bd557087d1f70a
MAQSOOD AHMAD AWAN <04051613018@student.qau.edu.pk>
2025-07-26 03:04:19 +0000

    feat:update migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -10,7 +10,6 @@ mod m20240101_000007_create_api_health_status_table;
 mod m20240101_000008_create_request_statistics_table;
 mod m20240101_000009_create_daily_statistics_table;
 mod m20240101_000010_insert_default_admin_data;
-mod m20240101_000011_insert_demo_request_statistics;
 
 pub struct Migrator;
 
@@ -28,7 +27,6 @@ impl MigratorTrait for Migrator {
             Box::new(m20240101_000008_create_request_statistics_table::Migration),
             Box::new(m20240101_000009_create_daily_statistics_table::Migration),
             Box::new(m20240101_000010_insert_default_admin_data::Migration),
-            Box::new(m20240101_000011_insert_demo_request_statistics::Migration),
         ]
     }
 }
